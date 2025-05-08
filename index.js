13e0fd830f569d24eb7a148f8d46de549f29d841
Rajvint <rajvint14@gmail.com>
2025-05-08 16:40:17 +0530

    added new tasks

--- index.js
+++ index.js
@@ -162,3 +162,36 @@ function purchase(){
    console.log("Something went wrong, please try again")
    errorParagraph.textContent = "Something went wrong, please try again"
 }
+
+// practice task 5 
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sum = document.getElementById("sum-el")
+// add function 
+function add(){
+   let result = num1 + num2
+   sum.textContent =  "sum: " + result
+}
+// add()
+
+// subtract function 
+function subtract(){
+   let result = num1 - num2
+   sum.textContent =  "sum: " + result
+}
+
+// divide function 
+function divide(){
+   let result = num1 / num2
+   sum.textContent =  "sum: " + result
+}
+
+// multiply function 
+function multiply(){
+   let result = num1 * num2
+   sum.textContent =  "sum: " + result
+}
+
+// practice task 6
